fba0423b38c5edbe0616a7dbe9cabe12574cd2c9
ff-dev-45 <flynn.fromont@gmail.com>
2021-03-20 23:47:11 +1300

    Included Javafx
    Added the javafx library to enable the Jeopardy.sh script to run easily

--- Jeopardy.sh
+++ Jeopardy.sh
@@ -1,1 +1,1 @@
-java --module-path /home/se2062020/javafx-sdk-11.0.2/lib --add-modules javafx.controls,javafx.media,javafx.base,javafx.fxml -jar Jeopardy.jar
+java --module-path ./javafx-11.0.2/lib --add-modules javafx.controls,javafx.media,javafx.base,javafx.fxml -jar Jeopardy.jar
